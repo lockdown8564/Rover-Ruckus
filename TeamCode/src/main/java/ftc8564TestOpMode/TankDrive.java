10fd05ba0023cbdf3a1b17dcce7453a9fefe8524
Margaret Li <margaretli2101@gmail.com>
2018-10-12 18:39:22 -0700

    tank drive teleop created

---
+++ TeamCode/src/main/java/ftc8564TestOpMode/TankDrive.java
@@ -0,0 +1,32 @@
+package ftc8564TestOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Tank Drive", group = "TeleOp")
+public class TankDrive extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+    }
+
+
+
+
+}
