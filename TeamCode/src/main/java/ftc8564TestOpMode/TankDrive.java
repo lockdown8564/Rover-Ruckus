09912ea4b38e9986d032e6c57b91eb242ee3db9e
andrewxmah <42054869+andrewxmah@users.noreply.github.com>
2018-10-12 19:31:20 -0700

    Tank Drive
    Basic Tank Drive AM

--- TeamCode/src/main/java/ftc8564TestOpMode/TankDrive.java
+++ TeamCode/src/main/java/ftc8564TestOpMode/TankDrive.java
@@ -8,19 +8,44 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TankDrive extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
+    private DcMotor left;
+    private DcMotor right;
+
+    private double scalePower(double dVal)
+    {
+        return (Math.signum(dVal) * ((Math.pow(dVal, 2) * (.9)) + .1));
+    }
+
     @Override
     public void init() {
 
+        left = hardwareMap.dcMotor.get("left");
+        right  = hardwareMap.dcMotor.get("right");
     }
 
     @Override
     public void start() {
-        runtime.reset();
 
+        runtime.reset();
     }
 
     @Override
     public void loop() {
+
+        if (abs(gamepad1.left_stick_y) > .2) {
+            left.setPower(-gamepad1.left_stick_y);
+        }
+
+        else {
+            left.setPower(0);
+        }
+
+        if (abs(gamepad1.right_stick_y) > .2) {
+            right.setPower(gamepad1.right_stick_y);
+        }
+
+        else {
+            right.setPower(0);
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.update();
